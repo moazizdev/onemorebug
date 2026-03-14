8e667d6e64cf51943a0646a0ca63beda5ee84a26
moazizdev <net_66@yahoo.com>
2026-03-14 23:43:10 +0200

    fix(config): correct typo in production baseUrl

--- config.production.php
+++ config.production.php
@@ -2,7 +2,7 @@
 
 return [
     'production' => true,
-    'baseUrl' => 'https://moazizdev.github.iop/onemorebug',
+    'baseUrl' => 'https://moazizdev.github.io/onemorebug',
     'title' => 'One more bug',
     'description' => 'موقع ومدونة محمد عبد العزيز | one more bug',
 ];
